644e47f031984cc5efcec728bf7ab1d3f0832fe4
Josh Dabuet <josiah@lightmedia.com.au>
2020-08-31 19:10:18 +0800

    Hero done

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html class="no-js">
+<html class="no-js" id="tabikana-page">
 	<head>
 		<meta charset="UTF-8">
 		<meta property="og:type" content="website" />
@@ -8,14 +8,25 @@
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<link rel="stylesheet" href="./public/assets/style.css">
 
+		<link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
+
 		<title>Tabikana</title>
 	</head>
 
 	<body>
-
-		<div class="hero">
-			HELLO WORLD
-		</div>
-
+		<header class="header">
+			<a class="header__logo" href="#">
+				<img src="./public/assets/images/logo.png" alt="">
+			</a>
+		</header>
+		<main>
+			<div class="hero">
+				<div class="hero__content">
+					<h1 class="hero__title">旅叶</h1>
+					<p class="hero__subtitle">project</p>
+					<p class="hero__description">諦めていた"旅行"を叶えるプロジェクト</p>
+				</div>
+			</div>
+		</main>
 	</body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,14 +2,14 @@ const { mix } = require('laravel-mix');
 
 const SRC_JS = 'src/js',
       SRC_CSS = 'src/sass',
-      // SRC_FONTS = 'src/fonts',
-      // SRC_IMAGES = 'src/images',
+      SRC_FONTS = 'src/fonts',
+      SRC_IMAGES = 'src/images',
       ASSETS_FOLDER = './public/assets';
 
 mix.sass(`${SRC_CSS}/style.scss`, ASSETS_FOLDER)
    // .js(`${SRC_JS}/app.js`, ASSETS_FOLDER)
    .options({
       processCssUrls: false
-   });
-   // .copyDirectory(SRC_FONTS, `${ASSETS_FOLDER}/fonts`)
-   // .copyDirectory(SRC_IMAGES, `${ASSETS_FOLDER}/images`);
+   })
+   .copyDirectory(SRC_FONTS, `${ASSETS_FOLDER}/fonts`)
+   .copyDirectory(SRC_IMAGES, `${ASSETS_FOLDER}/images`);
