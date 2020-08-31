b9955eadad779d62f2fb02cfdfbf82c7882f7f44
Josh Dabuet <josiah@lightmedia.com.au>
2020-08-31 21:58:16 +0800

    Index page 25% complete

--- index.php
+++ index.php
@@ -27,6 +27,68 @@
 					<p class="hero__description">諦めていた"旅行"を叶えるプロジェクト</p>
 				</div>
 			</div>
+			<div class="l-intro">
+				<div class="l-container">
+					<section class="intro">
+						<h2 class="section-title">
+							<span class="section-title__ja">はじめに</span>
+							<span class="section-title__en">First</span>
+						</h2>
+						<p class="intro__description">
+							今般の新型コロナウイルスの感染によりお亡くなりになられた方、<br>ご遺族の方々には心からお悔やみを申し上げます。<br>そして、現在も闘病中の患者さん、ご対応にあたっている関係者すべての皆様に<br>心よりお見舞いを申し上げます。<br><br>こんな大変な時だからこそ、これからの“医療”について提案させてください。<br>コロナ後に向けて。
+						</p>
+					</section>
+				</div>
+			</div>
+			<section class="l-overview">
+				<div class="l-overview__content">
+					<h2 class="section-title">
+						<span class="section-title__ja">プロジェクトの背景</span>
+						<span class="section-title__en">Background</span>
+					</h2>
+					<img class="l-overview__eyecatch" src="./public/assets/images/overview.jpg" alt="">
+					<div class="l-container">
+						<p class="l-overview__description">
+							年間140万人が亡くなる中、約100万人が “病室のベッド” で最期を迎えています。<br>つまり、あなたが最期にみる景色は、75%の確率で “病室の天井” になるかもしれません。<br><br>新型コロナウィルスにより、多くの人が “旅行へ行けない” 苦しみを経験しました。<br>しかし、コロナが流行するずっと前から、持病で旅行を諦めている人がたくさんいます。
+						</p>
+						<div class="why">
+							<p class="why__subtitle">解決したい課題</p>
+							<h3 class="why__title">
+								<span>なぜ</span>
+								「旅行に行けない」
+								<span>のか？</span>
+							</h3>
+							<div class="why__content u-flex">
+								<div class="why__block">
+									<div class="why__block-image">
+										<img src="./public/assets/images/i_traveler.png" alt="">
+									</div>
+									<h4 class="why__block-title">
+										【 旅行者の<span>不安</span> 】
+									</h4>
+									<p>どんな準備をすればいい...？</p>
+									<p>迷惑をかけてしまうかも...</p>
+									<p>体調悪化したらどうしよう...</p>
+								</div>
+								<div class="why__block">
+									<div class="why__block-image">
+										<img src="./public/assets/images/i_doctor.png" alt="">
+									</div>
+									<h4 class="why__block-title">
+										【 医療従事者の<span>不安</span> 】
+									</h4>
+									<p>誰に相談すればいいのだろう…</p>
+									<p>業務が忙しくて時間がない…</p>
+									<p>もし旅行中に何かあったらどうしよう…</p>
+								</div>
+								<div class="why__note">
+									どちらも“医療面”の不安を抱えています
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</main>
 	</body>
 </html>
