615b58a64289049da951a604c753c8f7e2e7b730
Josh Dabuet <josiah@lightmedia.com.au>
2020-08-31 17:25:25 +0800

    Init project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<meta property="og:type" content="website" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link rel="stylesheet" href="./public/assets/style.css">
+
+		<title>Tabikana</title>
+	</head>
+
+	<body>
+
+		<div class="hero">
+			HELLO WORLD
+		</div>
+
+	</body>
+</html>
